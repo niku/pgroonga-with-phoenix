a0a3b3b06fa39802e1c5d4572e3a8d8eb382b75d
niku <niku@niku.name>
2015-11-12 12:49:15 +0900

    Add routing for documents

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule DocumentSearch.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/documents", DocumentController
   end
 
   # Other scopes may use custom stacks.
